54959e1558706bd5c48250e175029ff72093ac5c
Simeon McGraw <simeonjmcgraw@gmail.com>
2016-04-12 12:30:58 -0400

    added comments

--- radicals.js
+++ radicals.js
@@ -20,52 +20,57 @@ function radsimplify(num) {
 	}
 	var result  = {},
 	    i;
+	// test possible perfect squares, starting from original value, going down to 2
 	for(i = num; i >= 2; i--) {
 		var sq = i*i;
-		if(num/sq % 1 === 0) {
+		if(num/sq % 1 === 0) { // if num/sq is an integer
 			result["coefficiant"] = i;
 			result["radical"] = num/sq;
 			return result;
 		}
 	}
+	// fallback, if radical cannot be simplified
 	result["coefficiant"] = 1;
 	result["radical"] = num;
 	return result;
 }
 
 submit.click(function(e) {
+	// retrieve float value, so we may give helpful user errors(ie "you must use integer values" etc)
 	var inputNum = parseFloat(input.val());
 	
 	try {
 		var simp,
 		    outputStr = "";
+		// get radsimplify object
 		simp = radsimplify(inputNum);
-		if(show.prop("checked")) {
+		if(show.prop("checked")) { // whether or not to show the simplification process
+			// add initial input value
 			outputStr = "√" + inputNum + "\n";
-			if(simp["coefficiant"] != 1) {
-				if(simp["radical"] != 1) {
+			if(simp["coefficiant"] == 1) { // if coefficiant is the implied 1, don't show it.
+				outputStr += "√" + simp["radical"];
+			} else {
+				if(simp["radical"] == 1) { // when radical is 1, that means the input was a perfect square
+					outputStr += simp["coefficiant"];
+				} else {
 					outputStr += "√" + (simp["coefficiant"] * simp["coefficiant"]) + "√" + simp["radical"] + "\n" +
 						         simp["coefficiant"] + "√" + simp["radical"];
-				} else {
-					outputStr += simp["coefficiant"];
 				}
-			} else {
-				outputStr += "√" + simp["radical"];
 			}
 		} else {
-			if(simp["coefficiant"] != 1) {
-				if(simp["radical"] != 1) {
-					outputStr = simp["coefficiant"] + "√" + simp["radical"];
-				} else {
-					outputStr = simp["coefficiant"];
-				}
-			} else {
+			if(simp["coefficiant"] == 1) { // if coefficiant is the implied 1, don't show it.
 				outputStr = "√" + simp["radical"];
+			} else {
+				if(simp["radical"] == 1) { // when radical is 1, that means the input was a perfect square
+					outputStr = simp["coefficiant"];
+				} else {
+					outputStr = simp["coefficiant"] + "√" + simp["radical"];
+				}
 			}
 		}
 		output.text(outputStr);
 	} catch(e) {
-		console.error(e);
+		// output any UserExceptions
 		if(e.message != null && e.name == "UserException") {
 			output.text("(" + e.message + ")");
 		}
